8eccda312c087751e6991cb1cb380f38eaba3173
GammaMillard <millard@gammatech.school>
2023-10-05 14:30:38 +0200

    commonJS change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import { scrappingData } from './scrapping/scrappingFunctions.js'
+const express = require( 'express')
+require( 'dotenv').config()
+const { scrappingData } = require( './scrapping/scrappingFunctions.js')
 
 const port = 8002
 const app = express()
 app.use(express.json())
 
+
 app.get('/', async(req,res) => {
-    
     const response = await scrappingData('https://empresite.eleconomista.es/Actividad/SOFTWARE-FACTORY/provincia/MADRID/')
     res.json({data : response })
-
-
 })
 
 app.listen(port , () => {
--- src/scrapping/scrappingFunctions.js
+++ src/scrapping/scrappingFunctions.js
@@ -1,7 +1,11 @@
-export const scrappingData = async(url) => {
+ const scrappingData = async(url) => {
     const response = await fetch(url,{
       
     })
     const text = await response.text()
     return text
+}
+
+module.exports = {
+    scrappingData
 }
